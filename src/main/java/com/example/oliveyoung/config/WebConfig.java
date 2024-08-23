7ac80ef597de2f8bd42555e4f8fbd4ebbf522f26
jaebinGit <tnwoql327@gmail.com>
2024-08-23 11:14:44 +0900

    Refactor : cors 경로 설정

--- src/main/java/com/example/oliveyoung/config/WebConfig.java
+++ src/main/java/com/example/oliveyoung/config/WebConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
+        registry.addMapping("https://api.hachwimu.com")
                 .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
