683d09436d3edc51564eb320fe31a5f7429018f9
jaebinGit <tnwoql327@gmail.com>
2024-08-25 02:29:10 +0900

    Refactor : redis 설정 수정

--- src/main/java/com/example/oliveyoung/config/RedisConfig.java
+++ src/main/java/com/example/oliveyoung/config/RedisConfig.java
@@ -1,41 +1,61 @@
 package com.example.oliveyoung.config;
 
-import com.fasterxml.jackson.annotation.JsonAutoDetect;
-import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.databind.jsontype.impl.LaissezFaireSubTypeValidator;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisClusterConfiguration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+
+import java.util.Arrays;
 
 @Configuration
 public class RedisConfig {
+
+    @Value("${SPRING_DATA_REDIS_HOST}")
+    private String redisHost;
+
+    @Value("${SPRING_DATA_REDIS_PORT}")
+    private int redisPort;
 
     @Bean
     public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
         RedisTemplate<String, Object> template = new RedisTemplate<>();
         template.setConnectionFactory(redisConnectionFactory);
 
-        // Key 직렬화기 설정 (String)
+        // Key는 String으로 직렬화
         template.setKeySerializer(new StringRedisSerializer());
 
         // JSON 직렬화를 위한 ObjectMapper 설정
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
         objectMapper.activateDefaultTyping(LaissezFaireSubTypeValidator.instance, ObjectMapper.DefaultTyping.NON_FINAL);
+        objectMapper.enable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
 
-        // Jackson2JsonRedisSerializer를 설정하고 ObjectMapper 적용
-        Jackson2JsonRedisSerializer<Object> jsonSerializer = new Jackson2JsonRedisSerializer<>(Object.class);
-        jsonSerializer = new Jackson2JsonRedisSerializer<>(objectMapper.getTypeFactory().constructType(Object.class));
+        // Jackson2JsonRedisSerializer에 ObjectMapper 설정
+        Jackson2JsonRedisSerializer<Object> serializer = new Jackson2JsonRedisSerializer<>(objectMapper, Object.class);
 
-        // Value와 Hash의 직렬화기 설정
-        template.setValueSerializer(jsonSerializer);
+        // Value와 Hash의 Value를 JSON으로 직렬화
+        template.setValueSerializer(serializer);
         template.setHashKeySerializer(new StringRedisSerializer());
-        template.setHashValueSerializer(jsonSerializer);
+        template.setHashValueSerializer(serializer);
 
         return template;
+    }
+
+    @Bean
+    public LettuceConnectionFactory redisConnectionFactory() {
+        RedisClusterConfiguration redisClusterConfiguration = new RedisClusterConfiguration(
+                Arrays.asList(redisHost + ":" + redisPort));
+
+        return new LettuceConnectionFactory(redisClusterConfiguration);
     }
 }
