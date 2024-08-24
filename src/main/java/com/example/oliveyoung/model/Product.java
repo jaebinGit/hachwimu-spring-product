c18edc072f4c5a63631b1505aedebe5cfe3d6073
jaebinGit <tnwoql327@gmail.com>
2024-08-24 21:29:25 +0900

    Refactor : JPA용 컬럼명 수정

--- src/main/java/com/example/oliveyoung/model/Product.java
+++ src/main/java/com/example/oliveyoung/model/Product.java
@@ -10,21 +10,44 @@ public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "name")
     private String name;
+
+    @Column(name = "image_url")
     private String imageUrl;
+
+    @Column(name = "price")
     private double price;
+
+    @Column(name = "brand")
     private String brand;
+
+    @Column(name = "is_best")
     private boolean isBest;
+
+    @Column(name = "delivery_info")
     private String deliveryInfo;
+
+    @Column(name = "sale_status")
     private boolean saleStatus;
 
     @Column(name = "coupon_status")
     private boolean couponStatus;
 
+    @Column(name = "gift_status")
     private boolean giftStatus;
+
+    @Column(name = "today_dream_status")
     private boolean todayDreamStatus;
+
+    @Column(name = "stock")
     private int stock;
+
+    @Column(name = "discount_price")
     private double discountPrice;
+
+    @Column(name = "other_discount")
     private boolean otherDiscount;
 
     public void purchase() {
