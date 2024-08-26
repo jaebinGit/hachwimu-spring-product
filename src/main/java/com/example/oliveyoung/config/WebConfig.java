9fab58f4cd423159558a4c6f3a14ee00bef647cd
jaebinGit <tnwoql327@gmail.com>
2024-08-26 13:19:25 +0900

    Refactor : healthCheck 구현

--- src/main/java/com/example/oliveyoung/config/WebConfig.java
+++ src/main/java/com/example/oliveyoung/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")  // API 경로를 지정
-                .allowedOrigins("*")
+                .allowedOrigins("*") //https://www.hachwimu.com
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(false);  // 자격 증명을 허용 (쿠키, 인증 헤더 전송)
---
+++ src/main/java/com/example/oliveyoung/controller/HealthCheckController.java
@@ -0,0 +1,22 @@
+package com.example.oliveyoung.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/products")
+public class HealthCheckController {
+
+    private static final Logger logger = LoggerFactory.getLogger(HealthCheckController.class);
+
+    // /products/health 요청을 처리하는 메서드
+    @GetMapping("/health")
+    public String productsHealthCheck() {
+        logger.info("Products에서 health check를 받았습니다.");
+        return "Products service is healthy!";
+    }
+}
